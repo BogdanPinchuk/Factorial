068355bf41f495867d0a8b12dc279e30e203a10c
Bogdan <pinchuk.brus@windowslive.com>
2020-09-03 13:15:21 +0300

    Add project files.

---
+++ Factorial.Test/Factorial_Test.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Factorial.Test
+{
+    [TestClass]
+    public class Factorial_Test
+    {
+        private delegate T del_Factorial<T, K>(K n);
+
+        [TestMethod]
+        public void CompareSpeedMethods()
+        {
+            
+        }
+    }
+}
---
+++ Factorial/Factorial.cs
@@ -0,0 +1,22 @@
+﻿#region Resources
+// 1. https://en.wikipedia.org/wiki/Factorial
+// 2. https://uk.wikipedia.org/wiki/Факторіал
+// 3. https://en.wikipedia.org/wiki/Gamma_function
+// 4. https://uk.wikipedia.org/wiki/Гамма-функція
+#endregion
+
+using System;
+
+namespace Factorial
+{
+    /// <summary>
+    /// Factorial number
+    /// </summary>
+    public class Factorial
+    {
+        private const string error = "Absolute values more than 70 are not accurate. You should use other method.";
+
+
+
+    }
+}
