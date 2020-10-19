87909d9bcd795f0409a883c0b1e034fa1f7156a2
Bogdan <pinchuk.brus@windowslive.com>
2020-10-19 13:35:07 +0300

    found max number for acurate calculate factorial number

--- Factorial.Test/Factorial_Test.cs
+++ Factorial.Test/Factorial_Test.cs
@@ -2,6 +2,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using System.Diagnostics;
 using System.Linq;
+using System.Numerics;
+using System.Text;
 
 namespace Factorial.Test
 {
@@ -9,6 +11,78 @@ namespace Factorial.Test
     public class Factorial_Test
     {
         private delegate T del_Factorial<T, K>(K n);
+
+        /// <summary>
+        /// Method for testing max value before add exception,
+        /// for use this method, you should comment "throw" block and change private -> public
+        /// </summary>
+        [TestMethod]
+        public void MaxNumberWithAcurateValue()
+        {
+            // For present result
+            var str = new StringBuilder("\nResult data:\n\n");
+
+            // maximum number for other formula which dependes on acurate of "double" type
+            uint maxN = 0;
+
+            BigInteger expected = default,
+                actual = default;
+
+            // for Ramanujan approximation power formula
+            for (maxN = 0; expected == actual; maxN++)
+            {
+                expected = Factorial.FactorialSlow(maxN);
+                actual = (BigInteger)Factorial.FactorialExp(maxN);
+            }
+
+            maxN -= 2;
+            expected = Factorial.FactorialSlow(maxN);
+            actual = (BigInteger)Factorial.FactorialExp(maxN);
+
+            str.Append("Acurate of Ramanujan approximation power formula:\n")
+                .Append($"\tmax number: {maxN};\n")
+                .Append($"\texpected value:\t{expected}\n")
+                .Append($"\tactual value:\t{actual}\n");
+
+            maxN++;
+            expected = Factorial.FactorialSlow(maxN);
+            actual = (BigInteger)Factorial.FactorialExp(maxN);
+
+            // next value
+            str.Append($"\n\tnext number: {maxN};\n")
+                .Append($"\texpected value:\t{expected}\n")
+                .Append($"\tactual value:\t{actual}\n");
+
+            expected = actual = default;
+
+            // for Ramanujan approximation exponent formula
+            for (maxN = 0; expected == actual; maxN++)
+            {
+                expected = Factorial.FactorialSlow(maxN);
+                actual = (BigInteger)Factorial.FactorialExp(maxN);
+            }
+
+            maxN -= 2;
+            expected = Factorial.FactorialSlow(maxN);
+            actual = (BigInteger)Factorial.FactorialExp(maxN);
+
+            str.Append("\nAcurate of Ramanujan approximation exponent formula:\n")
+                .Append($"\tmax number: {maxN};\n")
+                .Append($"\texpected value:\t{expected}\n")
+                .Append($"\tactual value:\t{actual}\n");
+
+            maxN++;
+            expected = Factorial.FactorialSlow(maxN);
+            actual = (BigInteger)Factorial.FactorialExp(maxN);
+
+            // next value
+            str.Append($"\n\tnext number: {maxN};\n")
+                .Append($"\texpected value:\t{expected}\n")
+                .Append($"\tactual value:\t{actual}\n");
+
+            // present
+            Debug.WriteLine(str);
+        }
 
         [TestMethod]
         public void CompareSpeedMethods()
@@ -34,5 +108,6 @@ namespace Factorial.Test
             timer.Stop();
             Debug.WriteLine($"Ramanujan approximation exponent: {timer.Elapsed.TotalMilliseconds} ms\n");
         }
+
     }
 }
--- Factorial/Factorial.cs
+++ Factorial/Factorial.cs
@@ -6,6 +6,7 @@
 #endregion
 
 using System;
+using System.Numerics;
 
 namespace Factorial
 {
@@ -14,7 +15,7 @@ namespace Factorial
     /// </summary>
     public class Factorial
     {
-        private const string error = "Absolute values more than 70 are not accurate. You should use other method.";
+        private const string error = "Absolute values more than 11 are not accurate. You should use other method.";
 
         /// <summary>
         /// Ramanujan approximation power
@@ -28,13 +29,13 @@ namespace Factorial
                 return 1;
             if (n == 2)
                 return 2;
-            if (n > 70)
+            if (n > 11)
                 throw new Exception(error);
 
             // parts of equation
             double sr = Math.Sqrt(2 * Math.PI * n),
                 pw = Math.Pow(n / Math.E, n),
-                fr = Math.Pow(1 + 1 / (2 * n) + 1 / (8 * n * n), 1 / 6);    // fraction
+                fr = Math.Pow(1 + 1 / (2.0 * n) + 1 / (8.0 * n * n), 1 / 6.0);    // fraction
 
             // Result
             double res = sr * pw * fr;
@@ -54,19 +55,52 @@ namespace Factorial
                 return 1;
             if (n == 2)
                 return 2;
-            if (n > 70)
+            if (n > 11)
                 throw new Exception(error);
 
             // parts of equation
             double sr = Math.Sqrt(2 * Math.PI * n),
                 pw = Math.Pow(n / Math.E, n),
-                fr = Math.Exp(1 / (12 * n) - 1 / (360 * n * n * n));
+                fr = Math.Exp(1 / (12.0 * n) - 1 / (360.0 * n * n * n));
 
             // Result
             double res = sr * pw * fr;
 
             return Math.Round(res, MidpointRounding.AwayFromZero);
         }
+
+
+        /// <summary>
+        /// Slow calculate but accurate result, can calculate absolute value more then 70
+        /// </summary>
+        /// <param name="n">value</param>
+        /// <returns>Factorial number</returns>
+        public static unsafe BigInteger FactorialSlow(uint n)
+        {
+            // absolutely value of n 
+            uint value = n;
+
+            // fast answer or start data for calculate
+            if (n == 0 || n == 1)
+                return 1;
+            else if (value == 2)
+                return 2;
+
+            // calculate
+            BigInteger result = 2;
+
+            for (uint i = 3; i <= value; i++)
+                result *= i;
+
+            return result;
+        }
+
+
+
+
+
+
+
 
     }
 }
