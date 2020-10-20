fe20680d621c7b512bb799d4140fec7a34e7f49b
Bogdan <pinchuk.brus@windowslive.com>
2020-10-20 15:45:33 +0300

    Create the some unit-tests

--- Factorial.Test/Factorial_Test.cs
+++ Factorial.Test/Factorial_Test.cs
@@ -1,9 +1,11 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+using System;
 using System.Diagnostics;
 using System.Linq;
 using System.Numerics;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Factorial.Test
 {
@@ -11,6 +13,247 @@ namespace Factorial.Test
     public class Factorial_Test
     {
         private delegate T del_Factorial<T, K>(K n);
+
+        private uint[] Stub
+            => Enumerable
+            .Range(0, 10)
+            .Select(t => (uint)t)
+            .ToArray();
+
+        private double[] Expected
+            => new double[]
+            {
+                1       ,
+                1       ,
+                2       ,
+                6       ,
+                24      ,
+                120     ,
+                720     ,
+                5040    ,
+                40320   ,
+                362880  ,
+                //3628800 ,
+                //39916800,
+            };
+        private ulong[] Big_Expected
+            => new ulong[]
+            {
+                1                  ,
+                1                  ,
+                2                  ,
+                6                  ,
+                24                 ,
+                120                ,
+                720                ,
+                5040               ,
+                40320              ,
+                362880             ,
+                3628800            ,
+                39916800           ,
+                479001600          ,
+                6227020800         ,
+                87178291200        ,
+                1307674368000      ,
+                20922789888000     ,
+                355687428096000    ,
+                6402373705728000   ,
+                121645100408832000 ,
+                2432902008176640000,
+                // max value for ulong is 18446744073709551615, so max number is 20
+            };
+
+
+        [TestMethod]
+        public void FactorialPow_OneInputValue()
+            => Factorial_OneInputValue(Factorial.FactorialPow);
+
+        [TestMethod]
+        public void FactorialExp_OneInputValue()
+            => Factorial_OneInputValue(Factorial.FactorialExp);
+
+        [TestMethod]
+        public void FactorialPow_ArrayValue()
+            => Factorial_ArrayValue(Factorial.FactorialPow);
+
+        [TestMethod]
+        public void FactorialExp_ArrayValue()
+            => Factorial_ArrayValue(Factorial.FactorialExp);
+
+        [TestMethod]
+        public void FactorialPow_Parallel()
+            => Factorial_ArrayValue(Factorial.FactorialPow);
+
+        [TestMethod]
+        public void FactorialExp_Parallel()
+            => Factorial_ArrayValue(Factorial.FactorialExp);
+
+        [TestMethod]
+        public void FactorialSlow_OneInputValue()
+            => Factorial_OneInputValue(Factorial.FactorialSlow);
+
+        //TODO: FactorialFast_OneInputValue()
+        //[TestMethod]
+        //public void FactorialFast_OneInputValue()
+        //    => Factorial_OneInputValue(Factorial.FibonacciFast);
+
+
+        private void Factorial_OneInputValue(del_Factorial<double, uint> fib)
+        {
+            // avarrage
+            var str = new StringBuilder("\nInput data:\n");
+            foreach (var i in Stub)
+                str.Append(i).Append("\t");
+
+            str.Append("\n\nExpected:\n");
+            foreach (var i in Expected)
+                str.Append(i).Append("\t");
+
+            // act
+            double[] actual = new double[Stub.Length];
+
+            for (int i = 0; i < Stub.Length; i++)
+                actual[i] = fib(Stub[i]);
+
+            str.Append("\n\nActual:\n");
+            foreach (var i in actual)
+                str.Append(i).Append("\t");
+
+            // assert
+            for (int i = 0; i < actual.Length; i++)
+                Assert.AreEqual(Expected[i], actual[i]);
+
+            Debug.WriteLine(str.ToString());
+        }
+
+        private void Factorial_ArrayValue(del_Factorial<double[], uint[]> fib)
+        {
+            // avarrage
+            var str = new StringBuilder("\nInput data:\n");
+            foreach (var i in Stub)
+                str.Append(i).Append("\t");
+
+            str.Append("\n\nExpected:\n");
+            foreach (var i in Expected)
+                str.Append(i).Append("\t");
+
+            // act
+            double[] actual = fib(Stub);
+
+            str.Append("\n\nActual:\n");
+            foreach (var i in actual)
+                str.Append(i).Append("\t");
+
+            // assert
+            for (int i = 0; i < actual.Length; i++)
+                Assert.AreEqual(Expected[i], actual[i]);
+
+            Debug.WriteLine(str.ToString());
+        }
+
+
+
+        [TestMethod]
+        public void FactorialPow_OneInputValue_Exception()
+            => Factorial_OneInputValueException_(Factorial.FactorialPow);
+
+        [TestMethod]
+        public void FactorialPow_ArrayValu_Exception()
+            => Factorial_ArrayValue_Exception(Factorial.FactorialPow);
+
+        [TestMethod]
+        public void FactorialExp_OneInputValue_Exception()
+            => Factorial_OneInputValueException_(Factorial.FactorialExp);
+
+        [TestMethod]
+        public void FactorialExp_ArrayValu_Exception()
+            => Factorial_ArrayValue_Exception(Factorial.FactorialExp);
+
+
+
+        private void Factorial_OneInputValueException_(del_Factorial<double, uint> fib)
+        {
+            // avarrage
+            uint[] stub = new uint[] { 12 };
+
+            string expected = "Absolute values more than 11 are not accurate. You should use other method.";
+
+            // act - assert
+            foreach (var i in stub)
+                Assert.ThrowsException<Exception>(() => fib(i), expected);
+        }
+
+        private void Factorial_ArrayValue_Exception(del_Factorial<double[], uint[]> fib)
+        {
+            // avarrage
+            uint[] stub = new uint[] { 12 };
+
+            string expected = "Absolute values more than 11 are not accurate. You should use other method.";
+
+            // act - assert
+            Assert.ThrowsException<Exception>(() => fib(stub), expected);
+        }
+
+
+        private void Factorial_OneInputValue(del_Factorial<BigInteger, uint> fib)
+        {
+            // avarrage
+            var str = new StringBuilder("\nInput data:\n");
+            foreach (var i in Stub)
+                str.Append(i).Append("\t");
+
+            str.Append("\n\nExpected:\n");
+            foreach (var i in Expected)
+                str.Append(i).Append("\t");
+
+            // act
+            BigInteger[] actual = new BigInteger[Stub.Length];
+
+            for (int i = 0; i < Stub.Length; i++)
+                actual[i] = fib(Stub[i]);
+
+            str.Append("\n\nActual:\n");
+            foreach (var i in actual)
+                str.Append(i).Append("\t");
+
+            // assert
+            for (int i = 0; i < actual.Length; i++)
+                Assert.AreEqual((BigInteger)Expected[i], actual[i]);
+
+            Debug.WriteLine(str.ToString());
+        }
+
+
+        [TestMethod]
+        public void Factorial_Slow_OneInputValue_Par_0_93()
+            => Factorial_OneInputValue_Par_0_20(Factorial.FactorialSlow);
+
+        //TODO: Factorial_Fast_OneInputValue_Par_0_20()
+        //[TestMethod]
+        //public void Factorial_Fast_OneInputValue_Par_0_20()
+        //    => Factorial_OneInputValue_Par_0_20(Factorial.FactorialFast);
+
+
+        private void Factorial_OneInputValue_Par_0_20(del_Factorial<BigInteger, uint> fib)
+        {
+            // avarrage
+            var str = new StringBuilder("\nResult data:\n\n");
+
+            // act
+            BigInteger[] actual = new BigInteger[Big_Expected.Length];
+
+            Parallel.For(0, Big_Expected.Length, (i) => actual[i] = fib((uint)i));
+
+            // assert
+            Parallel.For(0, Big_Expected.Length, (i) => Assert.AreEqual(Big_Expected[i], actual[i]));
+
+            // present
+            for (int i = 0; i < actual.Length; i++)
+                str.Append(i).Append(":\t").Append(actual[i]).Append("\n");
+
+            Debug.WriteLine(str);
+        }
+
 
         /// <summary>
         /// Method for testing max value before add exception,
@@ -23,21 +266,21 @@ namespace Factorial.Test
             var str = new StringBuilder("\nResult data:\n\n");
 
             // maximum number for other formula which dependes on acurate of "double" type
-            uint maxN = 0;
+            uint maxN;
 
             BigInteger expected = default,
                 actual = default;
 
-            // for Ramanujan approximation power formula
+            #region for Ramanujan approximation power formula
             for (maxN = 0; expected == actual; maxN++)
             {
                 expected = Factorial.FactorialSlow(maxN);
-                actual = (BigInteger)Factorial.FactorialExp(maxN);
+                actual = (BigInteger)Factorial.FactorialPow(maxN);
             }
 
             maxN -= 2;
             expected = Factorial.FactorialSlow(maxN);
-            actual = (BigInteger)Factorial.FactorialExp(maxN);
+            actual = (BigInteger)Factorial.FactorialPow(maxN);
 
             str.Append("Acurate of Ramanujan approximation power formula:\n")
                 .Append($"\tmax number: {maxN};\n")
@@ -46,16 +289,17 @@ namespace Factorial.Test
 
             maxN++;
             expected = Factorial.FactorialSlow(maxN);
-            actual = (BigInteger)Factorial.FactorialExp(maxN);
+            actual = (BigInteger)Factorial.FactorialPow(maxN);
 
             // next value
             str.Append($"\n\tnext number: {maxN};\n")
                 .Append($"\texpected value:\t{expected}\n")
                 .Append($"\tactual value:\t{actual}\n");
+            #endregion
 
             expected = actual = default;
 
-            // for Ramanujan approximation exponent formula
+            #region for Ramanujan approximation exponent formula
             for (maxN = 0; expected == actual; maxN++)
             {
                 expected = Factorial.FactorialSlow(maxN);
@@ -78,7 +322,8 @@ namespace Factorial.Test
             // next value
             str.Append($"\n\tnext number: {maxN};\n")
                 .Append($"\texpected value:\t{expected}\n")
-                .Append($"\tactual value:\t{actual}\n");
+                .Append($"\tactual value:\t{actual}\n"); 
+            #endregion
 
             // present
             Debug.WriteLine(str);
@@ -89,7 +334,7 @@ namespace Factorial.Test
         {
             // avarrage
             uint[] stub = Enumerable
-                .Range(0, 12)
+                .Range(0, 10)
                 .Select(t => (uint)t)
                 .ToArray();
 
@@ -116,5 +361,6 @@ namespace Factorial.Test
 
         }
 
+        // TODO: Extent the CompareSpeedMethods()
     }
 }
--- Factorial/Factorial.cs
+++ Factorial/Factorial.cs
@@ -17,7 +17,8 @@ namespace Factorial
     /// </summary>
     public class Factorial
     {
-        private const string error = "Absolute values more than 11 are not accurate. You should use other method.";
+        private const string errorP = "Absolute values more than 9 are not accurate. You should use other method.";
+        private const string errorE = "Absolute values more than 11 are not accurate. You should use other method.";
 
         /// <summary>
         /// Ramanujan approximation power formula
@@ -31,8 +32,8 @@ namespace Factorial
                 return 1;
             if (n == 2)
                 return 2;
-            if (n > 11)
-                throw new Exception(error);
+            if (n > 9)
+                throw new Exception(errorP);
 
             // parts of equation
             double sr = Math.Sqrt(2 * Math.PI * n),
@@ -50,11 +51,11 @@ namespace Factorial
         /// </summary>
         /// <param name="array">array value</param>
         /// <returns>array Factorial number</returns>
-        public static unsafe double[] FibonacciBine(uint[] array)
+        public static unsafe double[] FactorialPow(uint[] array)
         {
             // accurate
-            if (array.Max() > 11)
-                throw new Exception(error);
+            if (array.Max() > 9)
+                throw new Exception(errorP);
 
             // Square root of 2 * pi
             double sr2pi = Math.Sqrt(2 * Math.PI);
@@ -74,7 +75,7 @@ namespace Factorial
                 for (int i = 0; i < array.Length; i++, _f++, _s++, _t++, _a++, _r++)
                 {
                     // fast answer
-                    if (*_a == 0 || *_a == 1)
+                    if (*_a <= 1)
                     {
                         *_r = 1;
                         continue;
@@ -101,11 +102,11 @@ namespace Factorial
         /// </summary>
         /// <param name="array">array value</param>
         /// <returns>array Factorial number</returns>
-        public static double[] FibonacciBine_Par(uint[] array)
+        public static double[] FactorialPow_Par(uint[] array)
         {
             // accurate
-            if (array.Max() > 11)
-                throw new Exception(error);
+            if (array.Max() > 9)
+                throw new Exception(errorP);
 
             // Square root of 2 * pi
             double sr2pi = Math.Sqrt(2 * Math.PI);
@@ -120,21 +121,18 @@ namespace Factorial
                 else if (Math.Abs(array[i]) == 2)
                     res[i] = (array[i] > 0) ? 1 : -1;
 
-                double phi_n = Math.Pow(phi, array[i]);
+                double f_st = sr2pi * Math.Sqrt(array[i]),
+                    s_nd = Math.Pow(array[i] / Math.E, array[i]),
+                    t_rd = Math.Pow(1 + 1 / (2.0 * array[i]) + 1 / (8.0 * array[i] * array[i]), 1 / 6.0);
+
                 // Result
-                double result = (phi_n - Math.Pow(-1, array[i]) / phi_n) / sr5;
+                double result = f_st * s_nd * t_rd;
 
                 res[i] = Math.Round(result, MidpointRounding.AwayFromZero);
             });
 
             return res;
         }
-
-
-
-
-
-
 
         /// <summary>
         /// Ramanujan approximation exponent formula
@@ -149,7 +147,7 @@ namespace Factorial
             if (n == 2)
                 return 2;
             if (n > 11)
-                throw new Exception(error);
+                throw new Exception(errorE);
 
             // parts of equation
             double sr = Math.Sqrt(2 * Math.PI * n),
@@ -161,6 +159,98 @@ namespace Factorial
 
             return Math.Round(res, MidpointRounding.AwayFromZero);
         }
+
+        /// <summary>
+        /// Ramanujan approximation exponent formula
+        /// </summary>
+        /// <param name="array">array value</param>
+        /// <returns>array Factorial number</returns>
+        public static unsafe double[] FactorialExp(uint[] array)
+        {
+            // accurate
+            if (array.Max() > 11)
+                throw new Exception(errorE);
+
+            // Square root of 2 * pi
+            double sr2pi = Math.Sqrt(2 * Math.PI);
+
+            // some part of formula
+            double[] f_st = new double[array.Length],
+                s_nd = new double[array.Length],
+                t_rd = new double[array.Length],
+                res = new double[array.Length];
+
+            fixed (double* f = f_st, s = s_nd, t = t_rd, r = res)
+            fixed (uint* a = array)
+            {
+                double* _f = f, _s = s, _t = t, _r = r;
+                uint* _a = a;
+
+                for (int i = 0; i < array.Length; i++, _f++, _s++, _t++, _a++, _r++)
+                {
+                    // fast answer
+                    if (*_a <= 1)
+                    {
+                        *_r = 1;
+                        continue;
+                    }
+                    else if (*_a == 2)
+                    {
+                        *_r = 2;
+                        continue;
+                    }
+
+                    *_f = sr2pi * Math.Sqrt(*_a);
+                    *_s = Math.Pow(*_a / Math.E, *_a);
+                    *_t = Math.Pow(1 + 1 / (2.0 * *_a) + 1 / (8.0 * *_a * *_a), 1 / 6.0);
+                    *_t = Math.Exp(1 / (12.0 * *_a) - 1 / (360.0 * *_a * *_a * *_a));
+                    *_r = *_f * *_s * *_t;
+                    *_r = Math.Round(*_r, MidpointRounding.AwayFromZero);
+                }
+            }
+
+            return res;
+        }
+
+        /// <summary>
+        /// Ramanujan approximation exponent formula using parallel calculations
+        /// </summary>
+        /// <param name="array">array value</param>
+        /// <returns>array Factorial number</returns>
+        public static double[] FactorialExp_Par(uint[] array)
+        {
+            // accurate
+            if (array.Max() > 11)
+                throw new Exception(errorE);
+
+            // Square root of 2 * pi
+            double sr2pi = Math.Sqrt(2 * Math.PI);
+
+            double[] res = new double[array.Length];
+
+            Parallel.For(0, array.Length, (i) =>
+            {
+                // fast answer
+                if (Math.Abs(array[i]) <= 1)
+                    res[i] = 1;
+                else if (Math.Abs(array[i]) == 2)
+                    res[i] = (array[i] > 0) ? 1 : -1;
+
+                double f_st = sr2pi * Math.Sqrt(array[i]),
+                    s_nd = Math.Pow(array[i] / Math.E, array[i]),
+                    t_rd = Math.Pow(1 + 1 / (2.0 * array[i]) + 1 / (8.0 * array[i] * array[i]), 1 / 6.0);
+                    t_rd = Math.Exp(1 / (12.0 * array[i]) - 1 / (360.0 * array[i] * array[i] * array[i]));
+
+                // Result
+                double result = f_st * s_nd * t_rd;
+
+                res[i] = Math.Round(result, MidpointRounding.AwayFromZero);
+            });
+
+            return res;
+        }
+
+
 
 
         /// <summary>
@@ -174,7 +264,7 @@ namespace Factorial
             uint value = n;
 
             // fast answer or start data for calculate
-            if (n == 0 || n == 1)
+            if (n <= 1)
                 return 1;
             else if (value == 2)
                 return 2;
@@ -187,13 +277,6 @@ namespace Factorial
 
             return result;
         }
-
-
-
-
-
-
-
 
     }
 }
